c3578666eef2d7e2d0efa6dfe884ff33b480a0ce
ruchita00 <ruchitasagalgile04@gmail.com>
2021-05-11 09:01:39 +0530

    Added button.

--- my-app/src/components/DropDown.js
+++ my-app/src/components/DropDown.js
@@ -9,12 +9,12 @@ width: '112px',
 height: '40px',
 left: '517px',
 top: '256px',
+borderRadius: '8px',
 
 background: '#FFFFFF',
 border:' 1px solid #036FCB',
 boxSizing: 'border-box',
-boxShadow: '0px 2px 10px rgba(0, 0, 0, 0.08)',
-borderRadius: '8px',
+boxShadow: '0px 2px 10px rgba(0, 0, 0, 0.08)'
     }
 }))
 
@@ -22,8 +22,7 @@ const Dropdown=(props)=>{
     const {classes}=props;
    return(
        <div>
-       <div>
- 
+       <div> 
            <select className={classes.root}>       
               <option value="all status">All Status</option>
                <option value="active">Active</option>
